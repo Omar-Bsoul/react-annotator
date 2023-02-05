f638b38bcc8b4c2c6b1bda3229b743ec7edd312a
Omar-Bsoul <29519391+Omar-Bsoul@users.noreply.github.com>
2023-02-05 15:52:06 +0300

    1 file modified

--- components/ImageAnnotation.tsx
+++ components/ImageAnnotation.tsx
@@ -1,31 +1,50 @@
 import * as React from 'react';
 import { Image, Layer, Rect, Group } from 'react-konva';
-import {Box} from '@mui/material'
+import { Box } from '@mui/material';
 
 interface Props {
   imageSrc: string;
   classes: string[];
 }
 
-export const ImageAnnotation = ({ imageSrc, classes }) => {
+export const ImageAnnotation = (props: Props) => {
   const [shapes, setShapes] = React.useState<JSX.Element[]>([]);
 
   const handleDraw = (className: string) => {
-    // setShapes([
-    //   ...shapes, (<Rect
-    //     key={shapes.length}
-    //     x={20}
-    //     y={20}
-    //     width={50}
-    //     height={50}
-    //     fill={className}
-    //   />)
-    // ]);
+    setShapes([
+      ...shapes,
+      <Rect
+        key={shapes.length}
+        x={20}
+        y={20}
+        width={50}
+        height={50}
+        fill={className}
+      />,
+    ]);
   };
 
   return (
     <Box>
-     
-    <Box/>
+      <Image image={props.imageSrc} />
+      <Layer>
+        <Group>{shapes}</Group>
+      </Layer>
+      {props.classes?.map((className) => (
+        <button key={className} onClick={() => handleDraw(className)}>
+          Draw {className}
+        </button>
+      ))}
+    </Box>
   );
 };
+
+// <Image image={imageSrc} />
+// <Layer>
+//   <Group>{shapes}</Group>
+// </Layer>
+// {/* {classes?.map((className) => (
+//   <button key={className} onClick={() => handleDraw(className)}>
+//     Draw {className}
+//   </button>
+// ))} */}
