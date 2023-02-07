ad1f62b0b0e8999260eb5bc57dddd6cc8c94ba3f
Omar-Bsoul <29519391+Omar-Bsoul@users.noreply.github.com>
2023-02-07 10:22:17 +0300

    1 file modified

--- hooks/configuration.ts
+++ hooks/configuration.ts
@@ -1,24 +1,26 @@
 export const useConfiguration = () => {
+  const scaleFactor = 1;
+
   return {
     debounceDuration: 500,
-    minimumVertexHighlightDistance: 25,
-    minimumLineHighlightedDistance: 15,
+    minimumVertexHighlightDistance: 25 * scaleFactor,
+    minimumLineHighlightedDistance: 15 * scaleFactor,
     minimumShapeArea: 192,
     highlightedLineBackgroundColor: '#FFFFFF77',
-    highlightedLineBackgroundWidth: 6,
+    highlightedLineBackgroundWidth: 6 * scaleFactor,
     highlightedLineForegroundColor: '#000000',
-    highlightedLineForegroundWidth: 2,
+    highlightedLineForegroundWidth: 2 * scaleFactor,
     highlightedLineInsertVertexCircleBackgroundColor: '#FFFFFF',
-    highlightedLineInsertVertexCircleBorderWidth: 5,
+    highlightedLineInsertVertexCircleBorderWidth: 5 * scaleFactor,
     highlightedLineInsertVertexCircleForegroundColor: '#40c220',
-    highlightedLineInsertVertexCircleRadius: 5,
+    highlightedLineInsertVertexCircleRadius: 5 * scaleFactor,
     highlightedPointBackgroundColor: '#FFFFFF77',
-    highlightedPointBackgroundRadius: 3,
-    highlightedPointBackgroundWidth: 6,
+    highlightedPointBackgroundRadius: 3 * scaleFactor,
+    highlightedPointBackgroundWidth: 6 * scaleFactor,
     highlightedPointForegroundColor: '#000000',
-    highlightedPointForegroundRadius: 3,
+    highlightedPointForegroundRadius: 3 * scaleFactor,
     snackbarErrorDuration: 2000,
-    minimumCreateVertexDistance: 10,
-    minimumDisplaceVertexDistance: 5,
+    minimumCreateVertexDistance: 10 * scaleFactor,
+    minimumDisplaceVertexDistance: 5 * scaleFactor,
   };
 };
